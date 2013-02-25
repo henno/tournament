2a5a8cc3f499191ee24c62af8676cd67269b66b3
nalla <allan.korts@khk.ee>
2013-02-25 14:19:58 +0200

    parandatud viga,mille tõttu aadressi rea lõppu / jättes ei töötanud

--- modules/request.php
+++ modules/request.php
@@ -15,14 +15,15 @@ class request
 
 	function __construct()
 	{
-		if (isset($_SERVER['PATH_INFO'])){
-			if( $path_info = explode('/', $_SERVER['PATH_INFO']) ) {
+		if (isset($_SERVER['PATH_INFO'])) {
+			if ($path_info = explode('/', $_SERVER['PATH_INFO'])) {
 				array_shift($path_info);
 				$this->controller = isset($path_info[0]) ? array_shift($path_info) : DEFAULT_CONTROLLER;
-				$this->action     = isset($path_info[0]) ? array_shift($path_info) : 'index';
-				$this->params     = isset($path_info[0]) ? $path_info : null;
+				$this->action = isset($path_info[0]) && !empty($path_info[0]) ? array_shift($path_info) : 'index';
+				$this->params = isset($path_info[0]) ? $path_info : null;
 			}
 		}
 	}
 }
-$_request=new request;
+
+$_request = new request;
