a7814703caaf571b9719948cbe08f23d45753e84
Marleen Jürlau <menycet@gmail.com>
2013-02-27 14:30:45 +0200

    muudetud andmebaasi kasutaja

--- config.php
+++ config.php
@@ -9,7 +9,7 @@
 
 define('DEFAULT_CONTROLLER', 'welcome');
 define('DATABASE_HOSTNAME', 'localhost');
-define('DATABASE_USERNAME', 'root');
-define('DATABASE_PASSWORD', '');
+define('DATABASE_USERNAME', 'tournament');
+define('DATABASE_PASSWORD', 'YaEbVCeBcrjzPcQU');
 define('DATABASE_DATABASE', 'tournament');
 define('BASE_URL', '/tournament/');
