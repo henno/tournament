2c152238473a32560d7c38d8dd010b66e634e4db
nalla <allan.korts@khk.ee>
2013-03-12 11:41:50 +0200

    Add headings in tournament_add_view

--- views/tournaments_add_view.php
+++ views/tournaments_add_view.php
@@ -80,7 +80,7 @@
 	<div class="row-fluid">
 	<span style="display: inline-block;">
 	<div class="span6">
-
+		<h3>Turniiri üldandmed</h3>
 		<table id="tournament-attributes-table">
 			<tbody>
 			<tr>
@@ -155,6 +155,7 @@
 	</div>
 
 	<div class="span6">
+		<h3>Osalejad</h3>
 		<div style="width: 472px">
 			<table id="participants-table" class="table table-bordered table-striped" style="width: 472px !important;">
 				<thead>
