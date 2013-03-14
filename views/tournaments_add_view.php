d6879916c98bc1b62617afb5ce44c4e4f5bde57e
Kätlin Kilgi <katlin.kilgi@gmail.com>
2013-03-14 14:06:29 +0200

    Fix bug from last merge

--- views/tournaments_add_view.php
+++ views/tournaments_add_view.php
@@ -135,16 +135,16 @@
 			<tr>
 				<th>Turniiri algus</th>
 
-				<td><input type="text" class="datepicker" name="tournament[tournament_start]"
+				<td><input type="text" id="tournament_start" class="datepicker" name="tournament[tournament_start]"
 				           placeholder="pp.kk.aaaa hh:mm" autocomplete="off" onclick="
 					$(this).removeClass('viga')"></td>
-				
+
 			</tr>
 			<tr>
 				<th>Turniiri lõpp</th>
 				<td>
-					<input type="text" class="datepicker" id="tournament_end" name="tournament[tournament_end]"
-					       autocomplete="off" onclick="
+					<input type="text" class="datepicker" id="tournament_end"
+					       name="tournament[tournament_end]" autocomplete="off" onclick="
 					$(this).removeClass('viga')" placeholder="pp.kk.aaaa hh:mm"></td>
 			</tr>
 			<tr>
@@ -162,7 +162,8 @@
 			<tr>
 				<th>Mängu kestvus</th>
 				<td><input id="game-length" class="spinner" value="1" min="1" maxlength="2"
-				     name="tournament[tournament_game_time]" onkeypress="validate(event)" onclick="$(this).removeClass('viga')"></td>
+				           name="tournament[tournament_game_time]" onkeypress="validate(event)"
+				           onclick="$(this).removeClass('viga')"></td>
 			</tr>
 			<tr>
 				<th>Paus</th>
