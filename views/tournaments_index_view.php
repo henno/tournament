37728c6d7974a2548bdb5fdc0ec6f38fdc3113e3
nalla <allan.korts@khk.ee>
2013-03-25 16:51:33 +0200

    Add tournament removal functionality(#42)

--- views/tournaments_index_view.php
+++ views/tournaments_index_view.php
@@ -1,6 +1,6 @@
 <h2 class="form-signin-heading">Turniirid</h2>
 <p><a class="btn btn-large btn-primary" href="<?=BASE_URL?>tournaments/add">Lisa uus turniir</a></p>
-<table class="table table-bordered table-striped">
+<table id="tournaments-table" class="table table-bordered table-striped">
 	<thead>
 	<tr>
 		<th>
@@ -19,7 +19,7 @@
 	</thead>
 	<tbody>
 	<? if (! empty ($tournaments)): foreach ($tournaments as $tournament): ?>
-	<tr>
+	<tr id="tournament<?=$tournament['tournament_id'] ?>">
 		<td>
 			<?=$tournament['tournament_name']?>
 		</td>
@@ -33,7 +33,11 @@
 			<?="Vaata"?>
 			<a href="<?=BASE_URL?>tournaments/view/<?=$tournament['tournament_id']?>"><i
 					class="icon-pencil"></i></a>
+			<a href="<?= BASE_URL ?>tournaments/remove/<?=$tournament['tournament_id']?>"
+			   onclick="if (!confirm('Oled kindel?'))return false; remove_tournament_ajax
+				   (<?=$tournament['tournament_id']?>); return false">Kustuta <i class="icon-trash"></i></a>
 		</td>
+
 	</tr>
 		<? endforeach; endif ?>
 	</tbody>
