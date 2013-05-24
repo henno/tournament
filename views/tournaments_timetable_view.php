d2327bc357a582289163bdd2a5c8278b7053a591
karltonislaks <karl-tonis.laks@khk.ee>
2013-05-24 11:34:05 +0300

    Wrap times with input in timetable view [fixes #50442061]

--- views/tournaments_timetable_view.php
+++ views/tournaments_timetable_view.php
@@ -1,7 +1,11 @@
-<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.0/css/bootstrap-combined.min.css" rel="stylesheet">
-
+<html>
+<head>
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.0/css/bootstrap-combined.min.css" rel="stylesheet">
+	<meta charset="utf-8">
+</head>
+<body>
 <img src="http://tam.org.ee/blogs/skins/tam/images/toetajad/EMU_logo.jpg">
-<center><h1>Ajakava</h1></center>
+<h1 style="text-align: center">Ajakava</h1>
 <h2><?=$tournament['tournament_name']?></h2>
 <table class="table table-bordered table-striped">
 	<thead>
@@ -14,12 +18,14 @@
 	</tr>
 	</thead>
 	<? if (!empty ($games)): foreach ($games as $game): ?>
-	<tr>
-		<td>10:00</td>
-		<td><?=$game['participant_a_id']?></td>
-		<td><?=$game['participant_a_score']?>:<?=$game['participant_b_score']?></td>
-		<td><?=$game['participant_b_id']?></td>
-		<td>1</td>
-	</tr>
+		<tr>
+			<td><input value="10:00" type="text" style="width: 60px;float: left"></td>
+			<td><?=$game['participant_a_id']?></td>
+			<td><?=$game['participant_a_score']?>:<?=$game['participant_b_score']?></td>
+			<td><?=$game['participant_b_id']?></td>
+			<td>1</td>
+		</tr>
 	<? endforeach; endif ?>
 </table>
+</body>
+</html>
